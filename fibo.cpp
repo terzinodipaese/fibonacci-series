4b590c6d41ab0d013a2955bd9053b4aa034c0bff
mattinstruluz <30941518+terzinodipaese@users.noreply.github.com>
2019-09-13 01:54:57 -0700

    Update fibo.cpp

--- fibo.cpp
+++ fibo.cpp
@@ -1,7 +1,3 @@
-/*
- * 		Codice che risolve l'esercizio fibonacci del Coding contest
- */
-
 #include <iostream>
 #include <fstream>
 
@@ -21,30 +17,3 @@ long long int fibonacci(int n)
     
     return FIB[n];
 }
-
-
-
-
-int main(int argc, char** argv)
-{
-    int n;
-    ofstream out("output.txt");
-    ifstream in("input.txt");
-    
-    for(int i=0; i<100; i++)
-    {
-        in >> n;                        //read from file and memorise the value into our variable n
-        out << fibonacci(n) << endl;    //compute the value of fibonacci(n) and memorise it in the output file
-    }
-    
-    out.close();
-    in.close();
-        
-    return 0;
-}
-
-
-
-
-
-
